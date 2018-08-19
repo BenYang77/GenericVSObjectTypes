0b74337a1922cf896749b5cb93070fc68a1f8dc4
BenYang77 <Ben.Yang77@gmail.com>
2018-08-19 20:09:26 +0800

    Test Add Changing

--- GenericVSObjectTypes/GenericVSObjectTypes/About.aspx.cs
+++ GenericVSObjectTypes/GenericVSObjectTypes/About.aspx.cs
@@ -11,7 +11,7 @@ namespace GenericVSObjectTypes
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Write("View GitUpResult");
         }
     }
 }
